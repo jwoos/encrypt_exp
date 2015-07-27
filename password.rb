b9187a3d17c55cebf1a6b802d88edb06fb71e558
Jun Woo Shin <shin.junwoo97@gmail.com>
2015-07-26 21:20:20 -0400

    encryption works

--- password.rb
+++ password.rb
@@ -1,21 +1,28 @@
 require 'openssl'
-require 'colorize'
+require 'base64'
 
-key = ARGV[0]
-iv = ARGV[1]
-name = ARGV[2]
+name = ARGV[0]
 
-in_file = File.path(name + ".txt")
-input_data = IO.readlines(in_file)
+in_file = File.path(name)
+input_data = IO.readlines(in_file)[1].chomp
 
-data = input_data[1]
-
-cipher = OpenSSL::Chipher.new('AES-256-CBC')
+cipher = OpenSSL::Cipher.new('AES-256-CBC')
 cipher.encrypt
-encrypted = cipher.update(data)
+encrypted = cipher.update(input_data) + cipher.final
 
-out_file = File.path(name + "-encrypted.txt")
-File.open(filename, "w") do |x|
-  p input_data[0]
-  p
+key_file = File.path("key")
+iv_file = File.path("iv")
+cipher.key = IO.readlines(key_file)[0].chomp
+cipher.iv = IO.readlines(iv_file)[0].chomp
+
+out_file = File.path(name + "-encrypted")
+File.open(out_file, "w") do |x|
+  x << IO.readlines(in_file)[0]
+  x << Base64.encode64(encrypted)
 end
+
+base64_encrypted = Base64.encode64(encrypted)
+base64_decrypted = Base64.decode64(base64_encrypted)
+
+p  "ID: #{IO.readlines(in_file)[0].tr("\n", "")}"
+p "PW: #{encrypted}"
